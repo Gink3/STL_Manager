be208c06aedf5e1a5d2821362282856c2d78b683
Taylor King <43785015+Gink3@users.noreply.github.com>
2023-12-21 13:33:13 -0600

    Added functionality to double click on list to navigate directories

--- main.py
+++ main.py
@@ -39,19 +39,32 @@ class MainWindow(QMainWindow):
       self.filemap = self.parser.list_model_files(self.parser.get_root_path())
 
       self.setWindowTitle("STL Manager")
-      list_widget = QListWidget()
-      list_widget.addItems(self.filemap.keys())
+      self.list_widget = QListWidget()
+      self.list_widget.addItems(self.filemap.keys())
 
-      list_widget.currentItemChanged.connect(self.index_changed)
-      list_widget.currentTextChanged.connect(self.text_changed)
+      # self.list_widget.currentItemChanged.connect(self.index_changed)
+      # self.list_widget.currentTextChanged.connect(self.text_changed)
+      self.list_widget.itemDoubleClicked.connect(self.double_click_file)
 
-      self.setCentralWidget(list_widget)
+      self.setCentralWidget(self.list_widget)
 
-   def index_changed(self, i): # Not an index, i is a QListWidgetItem
-      print(i.text())
+   # def index_changed(self, i): # Not an index, i is a QListWidgetItem
+   #    logging.debug("index_changed " + i.text())
 
-   def text_changed(self, s): # s is a str
-      print(s)
+   # def text_changed(self, s): # s is a str
+   #    logging.debug("text_changed " + s)
+
+   def double_click_file(self, s):
+      logging.debug("Double clicked on " + s.text())
+      # if selected text if a directory
+      if s.text().endswith("\\") or s.text() == "..":
+         # regenerate filemap
+         new_filemap = self.parser.list_model_files(self.filemap[s.text()])
+         self.filemap = new_filemap
+
+         # clear list widget and add new elements
+         self.list_widget.clear()
+         self.list_widget.addItems(self.filemap.keys())
 
 if __name__ == "__main__":
    logging.basicConfig(filename='stl_manager.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
