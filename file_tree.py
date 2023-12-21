50b15c00de0885705c52f27a18d46d009f6b951c
Taylor King <43785015+Gink3@users.noreply.github.com>
2023-12-21 11:41:40 -0600

    Added initial file tree

---
+++ file_tree.py
@@ -0,0 +1,60 @@
+import os
+
+
+class FileTreeParser:
+   def __init__(self):
+      self.file_dict = dict()
+      self.num_node_parsed = 0
+
+   def get_root_path(self):
+      root = input("Root Path: ")
+
+      # Check root string is not empty
+      if not root:
+         print("Error: Root Path is empty")
+
+      return root
+
+
+   def check_duplicates(self):
+      # define root file path
+      root = self.get_root_path()
+      print("Parsing for duplicate .stl files from \"" + root + "\"")
+      list_dir = os.listdir(root)
+
+      # Iterate through the list of directories
+      for node in list_dir:
+         self.num_node_parsed += 1
+         next_node = os.path.join(root, node)
+         
+         if node.endswith(".stl"):
+            print("Parsing: {}".format(next_node))
+
+         # If a directory add to the list of directories to go through
+         if os.path.isdir(next_node):
+            # Check if filepath is a duplicate
+            # Shouldn't happen, but want to be safe
+            if not next_node in list_dir:
+               list_dir.append(next_node)
+         # # If a file
+         # else:
+         #    # If file is an stl file, check to add into the dict
+         #    if node.endswith(".stl"):
+         #       if not self.file_dict.keys().contains(node):
+         #          self.file_dict[node] = 1
+         #       else: 
+         #          self.file_dict[node] += 1
+         #    # If not an stl file, ignore
+         #    else:
+         #       continue
+         
+      print(self.file_dict)
+      print("Number of Nodes Parsed: {}".format(self.num_node_parsed))
+
+
+
+
+if __name__ == "__main__":
+   parser = FileTreeParser()
+
+   parser.check_duplicates()
