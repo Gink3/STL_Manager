85f404f9a42e200cfb06c3bab74f4760e268dc7e
Taylor King <43785015+Gink3@users.noreply.github.com>
2023-12-21 12:37:46 -0600

    Moved qt window to its own class

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 import configparser
+import sys
+
+# https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
+import logging
+
 from file_tree import FileTreeParser 
 
 # https://build-system.fman.io/pyqt5-tutorial
-from PyQt5.QtWidgets import QApplication, QLabel
+# https://www.pythonguis.com/tutorials/pyqt-basic-widgets/
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDoubleSpinBox,
+    QLabel,
+    QLineEdit,
+    QListWidget,
+    QMainWindow,
+    QSlider,
+    QSpinBox,
+)
 
+class MainWindow(QMainWindow):
+   def __init__(self):
+      super(MainWindow, self).__init__()
+
+      self.config = configparser.ConfigParser()
+      self.config.read('config.txt')
+      print(self.config['DEFAULT']['LibraryRoot'])
+      self.parser = FileTreeParser(self.config['DEFAULT']['LibraryRoot'])
+
+      self.setWindowTitle("STL Manager")
+      widget = QListWidget()
+      widget.addItems(["One", "Two", "Three"])
+
+      widget.currentItemChanged.connect(self.index_changed)
+      widget.currentTextChanged.connect(self.text_changed)
+
+      self.setCentralWidget(widget)
+
+   def index_changed(self, i): # Not an index, i is a QListWidgetItem
+      print(i.text())
+
+   def text_changed(self, s): # s is a str
+      print(s)
 
 if __name__ == "__main__":
-   # Initialize config parser
-   config = configparser.ConfigParser()
-   config.read('config.txt')
-   print(config['DEFAULT']['LibraryRoot'])
-   parser = FileTreeParser(config['DEFAULT']['LibraryRoot'])
 
    # Basic QT window
-   app = QApplication([])
-   label = QLabel('STL Manager')
-   label.show()
+   app = QApplication(sys.argv)
+   window = MainWindow()
+   window.show()
    app.exec()
